8690ba8cd5e1ae3843c2b91736247f56148920e6
CalebStanberry <calebstanberry@gmail.com>
2026-03-28 12:57:19 -0400

    Basic flask with tailwind

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=int(os.getenv("PORT", "5050")), debug=True)
