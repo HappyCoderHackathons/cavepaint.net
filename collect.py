77f92a1035025a917da61c1ed00604e505c05223
Tommy M <sterlingtorrington@gmail.com>
2026-03-29 05:52:53 -0400

    train

--- collect.py
+++ collect.py
@@ -20,7 +20,7 @@ import cv2
 import mediapipe as mp
 import numpy as np
 
-GESTURES = ["open_hand", "point", "fist", "peace"]
+GESTURES = ["open_hand", "point", "fist", "peace", "gun"]
 CYCLES = 5
 COUNTDOWN_SECONDS = 3
 RECORD_SECONDS = 8
